12d6561be4cf1037c0a1b5f169786540ff28c464
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-09 23:31:00 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,6 @@
 def my_collect(languages)
-  i = 0
-  while i < languages.length
-    yield(languages[i])
-    i = i + 1
-    # languages.each do |lang|
-    #   yield(lang)
+  languages.each do |lang|
+    lang.upcase
 end
   languages
 end
